22b4085aea6df61200a72b7cba1e6d759bb5420b
Вениамин <ven200472@gmail.com>
2025-06-28 01:58:32 +0300

    создал tags., ingrediends

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name='Название ингредиента'
+    )
+    measurement_unit = models.CharField(
+        max_length=100, verbose_name='Единица измерения'
+    )
+
+    class Meta:
+        verbose_name = 'ингредиент'
+        verbose_name_plural = 'ингредиенты'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
--- backend/tags/models.py
+++ backend/tags/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=32, verbose_name='азвание тега'
+    )
+    slug = models.SlugField(
+        max_length=32, unique=True, verbose_name='Ключ тега'
+    )
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
